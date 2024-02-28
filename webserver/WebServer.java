8881c1d9f341be613ffda3c0f2a15e61379ff03f
Zachary Brown <brownz4@mail.uc.edu>
2024-02-28 07:52:31 -0500

    WebServer

--- webserver/WebServer.java
+++ webserver/WebServer.java
@@ -15,8 +15,8 @@ public final class WebServer
         int port = 6789;
 
         // Establish the listen socket.
-        String server = "www.javaworld.com";
-        System.out.println( "Loading contents of URL: " + server );
+        // String server = "www.javaworld.com";
+        // System.out.println( "Loading contents of URL: " + server );
         ServerSocket serverSocket = new ServerSocket( port);
 
         // Process HTTP service requests in an infinite loop.
@@ -31,7 +31,7 @@ public final class WebServer
             // Start the thread.
             thread.start();
         }   
-}
+    }
 }
 
 final class HttpRequest implements Runnable
@@ -71,13 +71,92 @@ final class HttpRequest implements Runnable
         // Get and display the header lines.
         String headerLine = null;
         while ((headerLine = br.readLine()).length() != 0) {
-        System.out.println(headerLine);
+            System.out.println(headerLine);
         }
+
+        StringTokenizer tokens = new StringTokenizer(requestLine);
+        tokens.nextToken();
+
+        
+
+        String fileName = tokens.nextToken();
+        
+
+        // if ( fileName.startsWith("/")) {
+        //     fileName = fileName.substring(1,fileName.length());
+        // }
+        fileName = "." + fileName;  
+
+        System.out.println(fileName);
+        FileInputStream fis = null;
+        boolean fileExists = true;
+        try {
+            fis = new FileInputStream( fileName );
+        } catch (FileNotFoundException e) {
+            fileExists = false;
+        }
+
+        String statusLine = null;
+        String contentTypeLine = null;
+        String entityBody = null;
+
+        if (fileExists) {
+            statusLine = "HTTP/1.1 200 OK " + CRLF;
+            contentTypeLine = "Content-type: " + contentType( fileName );
+        } else {
+            statusLine = "HTTP/1.0 404 Not Found" + CRLF;
+            contentTypeLine = "NONE";
+            entityBody = "\n\n Not Found";
+        }
+
+        os.writeBytes( statusLine );
+
+        os.writeBytes( contentTypeLine );
+
+        // os.writeBytes( entityBody );
+
+        os.writeBytes( CRLF );
+
+        if ( fileExists ) {
+            writeBytes( fis, os );
+            fis.close();
+        } else {
+            os.writeBytes( entityBody );
+        }
+
+
         
         // Close streams and socket.
         os.close();
         br.close();
         socket.close();
     }
+
+    public static String contentType(String fileName) {
+        if ( fileName.endsWith(".htm") || fileName.endsWith( ".html" ) ) {
+            return "text/html";
+        }
+        else if ( fileName.endsWith(".gif") ) {
+            return "image/gif";
+        }
+        else if ( fileName.endsWith(".jpg") || fileName.endsWith( ".jpeg" ) ) {
+            return "image/jpeg";
+        }
+        else if ( fileName.endsWith(".txt") ) {
+            return "text/plain";
+        }
+        return "application/octet-stream";
+    }
+
+    private static void writeBytes(FileInputStream fis, OutputStream os ) throws Exception {
+        byte[] buffer = new byte[1024];
+        int bytes = 0;
+
+        while ( ( bytes = fis.read( buffer ) ) != -1 ) {
+            os.write( buffer, 0, bytes );
+        }
+    }
 }
+
+
 
